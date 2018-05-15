ea0dd35bc8095cf2ebeabaa496b6373288f49347
namx-holi <chucktesta@live.com.au>
2018-05-15 13:02:44 +1000

    Added colour test

--- test.c
+++ test.c
@@ -9,13 +9,20 @@
 #endif
 
 
+#ifndef COLOUR_H
+#include "colour.h"
+#define COLOUR_H
+#endif
+
 
 void test_random(void);
+void test_colour(void);
 
 
 int main(void)
 {
 	test_random();
+	test_colour();
 
 	return 0;
 }
@@ -35,6 +42,24 @@ void test_random(void)
 	{
 		printf("%d\n", rand_int(-5, 5));
 	}
+
+	printf("\n");
+}
+
+
+void test_colour(void)
+{
+	printf("Running test_colours\n");
+
+	colour_printf("red", "this is red text");
+	colour_printf("green", " and this is green!\n");
+
+	colour_printf("yellow", "this is yellow wow!");
+	colour_printf("blue", " and this is blue?");
+	colour_printf("magenta", " now this is magenta\n");
+
+	colour_printf("cyan", "this is cyan,");
+	colour_printf("white", " and this is white\n");
 
 	printf("\n");
 }
