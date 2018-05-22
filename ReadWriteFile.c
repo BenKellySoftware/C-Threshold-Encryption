404ed4dbc3ddff49ac285ab82459a193fcf8f98a
lilileah <12197761@student.uts.edu.au>
2018-05-22 13:01:11 +1000

    Few coding style changes

--- ReadWriteFile.c
+++ ReadWriteFile.c
@@ -2,12 +2,15 @@
 #include <string.h>
 #include <stdlib.h>
 
-int main (){
+int main (void)
+{
 	FILE * fPointer;
-	fPointer = fopen("NewFile", "wb");
+	fPointer = fopen("NewFile", "wb");/*Creates and opens new file where image text will be written*/
 
 	FILE *file_p;
-	file_p = fopen("black.bmp", "rb");
+	file_p = fopen("FileToBeConverted", "rb");/*Opens image file that is being converted to text*/
+
+	/*Next 4 lines are used to find the size of file */
 	fseek(file_p, 0L, SEEK_END);/*Go to the end of this file*/
 	int size = ftell(file_p);/* get the position we are in the file as a number of bytes */
 	rewind(file_p);/* go back to the start of the file so we can read it */
@@ -26,5 +29,4 @@ int main (){
 	fclose(file_p);
 
 	return 0; 
-
 }
