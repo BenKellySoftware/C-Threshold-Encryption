34a98038cff5cc6d2abd68654768f3ccd5da16fd
lilileah <12197761@student.uts.edu.au>
2018-05-28 13:49:51 +1000

    few changes

--- main.c
+++ main.c
@@ -57,33 +57,36 @@ int retrieve_recipe(char *filename, point_t a, point_t b, point_t c)
     scanf("%s\n", &b2);
     printf("code 3>>");
     scanf("%s\n", &c3);
-    printf("Thank you!")
+    printf("Thank you!");
 
     if ( a == a1 && b =b2 && c =c3)
-
-    FILE * fPointer;
-    fPointer = fopen("smile.png", "wb"); 
-
-    FILE *file_p;
-    file_p = fopen("NewFile", "rb");
-    fseek(file_p, 0L, SEEK_END);/*Go to the end of this file*/
-    int size = ftell(file_p);/* get the position we are in the file as a number of bytes */
-    rewind(file_p);/* go back to the start of the file so we can read it */
-    printf("%d\n", size);/*Prints size for da refererence*/
-
-    /*For loop for the size of file*/   
-    int i;
-    for ( i = 0; i < size; i++)
+    /*THis part is not finished, im just a little confused about what i do 
+    with these numbers???? and also can they put in more than 3 codes? */
     {
-        char* b = (char*)malloc(sizeof(char)*size);
-        fread(b, sizeof(char), size, file_p);
-        fwrite(b, sizeof(char), size, fPointer);
+        FILE * fPointer;
+        fPointer = fopen("smile.png", "wb"); 
+
+        FILE *file_p;
+        file_p = fopen("NewFile", "rb");
+        fseek(file_p, 0L, SEEK_END);/*Go to the end of this file*/
+        int size = ftell(file_p);/* get the position we are in the file as a number of bytes */
+        rewind(file_p);/* go back to the start of the file so we can read it */
+        printf("%d\n", size);/*Prints size for da refererence*/
+
+        /*For loop for the size of file*/   
+        int i;
+        for ( i = 0; i < size; i++)
+        {
+            char* b = (char*)malloc(sizeof(char)*size);
+            fread(b, sizeof(char), size, file_p);
+            fwrite(b, sizeof(char), size, fPointer);
+        }
+
+        fclose(fPointer);
+        fclose(file_p);
+
+        return 0; 
     }
-
-    fclose(fPointer);
-    fclose(file_p);
-
-    return 0; 
 
 }
 
